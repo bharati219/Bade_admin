7aadcd894161c7c3857fb7330cae3cc192b97e56
bharati219 <bharatij219@gmail.com>
2025-04-17 16:29:33 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 var util = require("util");
 var conn = mysql.createConnection({
-    "host":"localhost",
-    "user":"root",
-    "password":"",
-    "database":"bade_hospital"
+    "host":"byxjmh2nfvcpwneihzue-mysql.services.clever-cloud.com",
+    "user":"uakc3j5k7dkc2ynw",
+    "password":"ros2nx2RRLJYe673Gb1S",
+    "database":"byxjmh2nfvcpwneihzue"
 });
 
 var exe = util.promisify(conn.query).bind(conn);
